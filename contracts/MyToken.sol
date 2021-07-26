6e26c1ee6e31a6b0da975c1b682defe27cbe978d
Daksh <jo.dakshjoshi@gmail.com>
2021-07-26 14:39:20 +0530

    initial commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,9 +2,30 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20Capped {
-    constructor() ERC20("Daksh", "DJ", 18, 1000000) ERC20Capped(1000000) {
+contract MyToken is ERC20Capped, Ownable {
+    constructor() ERC20("Daksh", "DJ", 18, 1000000) ERC20Capped(10000000) {
         _mint(msg.sender, 1000000);
+    }
+
+    function mint(uint256 amount) public virtual onlyOwner {
+        _mint(msg.sender, amount);
+    }
+
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+    function burnFrom(address account, uint256 amount) public virtual {
+        uint256 currentAllowance = allowance(account, _msgSender());
+        require(
+            currentAllowance >= amount,
+            "ERC20: burn amount exceeds allowance"
+        );
+        unchecked {
+            _approve(account, _msgSender(), currentAllowance - amount);
+        }
+        _burn(account, amount);
     }
 }
