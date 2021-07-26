039935e87ead3e3b9e7288f4eca59b024742fdc9
Daksh <jo.dakshjoshi@gmail.com>
2021-07-26 14:20:36 +0530

    initial commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("Daksh", "DJ", 18, 1000000) {
+contract MyToken is ERC20Capped {
+    constructor() ERC20("Daksh", "DJ", 18, 1000000) ERC20Capped(1000000) {
         _mint(msg.sender, 1000000);
     }
 }
